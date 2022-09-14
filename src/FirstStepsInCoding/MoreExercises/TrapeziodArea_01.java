f7deeb3f99329408793e6880f356a54acf32ffcd
DeyanFilipov <111912445+DeyanFilipov@users.noreply.github.com>
2022-09-14 13:16:24 +0300

    Update TrapeziodArea_01.java

--- src/FirstStepsInCoding/MoreExercises/TrapeziodArea_01.java
+++ src/FirstStepsInCoding/MoreExercises/TrapeziodArea_01.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class TrapeziodArea_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int b1 = Integer.parseInt(scanner.nextLine());
-        int b2 = Integer.parseInt(scanner.nextLine());
+        double b1 = Double.parseDouble(scanner.nextLine());
+        double b2 = Double.parseDouble(scanner.nextLine());
         double h = Double.parseDouble(scanner.nextLine());
-        double Area = (b1 + b2) * h / 2;
-        System.out.println(Area);
+        double area = (b1 + b2) * h / 2;
+        System.out.printf("%.2f", area);
     }
 }
+
